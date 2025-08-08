23dce47cb1be21be20391c893aebe7b61f35e67b
abolfazl mashhadi <abolfazllmash@gmail.com>
2025-08-08 07:31:18 +0000

    سعی کن از لحاظ ui کمی شرایط پروفایل رو بهبود بدی

--- src/components/ProfilePageContent.tsx
+++ src/components/ProfilePageContent.tsx
@@ -74,7 +74,7 @@ export default function ProfilePageContent() {
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="md:col-span-2 space-y-8">
+            <div className="md:col-span-2">
                 <Card>
                     <CardHeader>
                         <CardTitle>اطلاعات کاربری</CardTitle>
